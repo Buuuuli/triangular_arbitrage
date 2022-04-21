eb4e96fb63834d076ca77913c9e58917c94a66bc
Gary Xu <garyxu@outlook.com>
2022-04-21 10:40:50 -0400

    use ib to get forex data

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from ibapi.contract import Contract
 
 import threading
 import time
+import pandas
 
 
 class IBapi(EWrapper, EClient):
@@ -21,7 +22,7 @@ def run_loop():
 
 
 app = IBapi()
-app.connect('127.0.0.1', 7497, 123)
+app.connect('127.0.0.1', 7497, 10645)
 
 # Start the socket in a thread
 api_thread = threading.Thread(target=run_loop, daemon=True)
@@ -29,24 +30,32 @@ api_thread.start()
 
 time.sleep(1)  # Sleep interval to allow time for connection to server
 
-# Create contract object
-eurusd_contract = Contract()
-eurusd_contract.symbol = 'EUR'
-eurusd_contract.secType = 'CASH'
-eurusd_contract.exchange = 'IDEALPRO'
-eurusd_contract.currency = 'USD'
+
+def create_contract(curr1, curr2):
+    # Create contract object
+    contract = Contract()
+    contract.symbol = curr1
+    contract.secType = 'CASH'
+    contract.exchange = 'IDEALPRO'
+    contract.currency = curr2
+    return contract
+
+
+EUR_USD_contract = create_contract('EUR', 'USD')
+
+
+def request_and_append_data(contract):
+    app.reqHistoricalData(reqId=1, contract=contract, endDateTime='', durationStr='1 D', barSizeSetting='1 day',
+                          whatToShow='MIDPOINT', useRTH=0, formatDate=1, keepUpToDate=False, chartOptions=[])
+
 
 # Request historical candles
-app.reqHistoricalData(1, eurusd_contract, '', '2 D', '1 hour', 'BID', 0, 2, False, [])
 
-time.sleep(5)  # sleep to allow enough time for data to be returned
 
-# Working with Pandas DataFrames
-import pandas
+time.sleep(3)  # sleep to allow enough time for data to be returned
 
 df = pandas.DataFrame(app.data, columns=['DateTime', 'Close'])
-df['DateTime'] = pandas.to_datetime(df['DateTime'], unit='s')
-df.to_csv('EURUSD_Hourly.csv')
+df['DateTime'] = pandas.to_datetime(df['DateTime'], format='%y%m%d')
 
 print(df)
 
