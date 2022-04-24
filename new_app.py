6edd3b23bdeb959ef17bfddd820a8e1566b01f30
Buuuuuli <km509@duke.edu>
2022-04-24 14:36:18 -0400

    mac

--- new_app.py
+++ new_app.py
@@ -1,4 +1,3 @@
-
 import dash
 import ibapi.wrapper
 import pandas as pd
@@ -18,13 +17,10 @@ from ibapi.contract import Contract
 from ibapi.order import Order
 import time
 import threading
-import dash_table as dt
-
-
-
+from dash import dash_table
+import base64
 
 from function import *
-
 
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
@@ -36,42 +32,45 @@ import time
 import pandas
 from pandas import DataFrame
 
-
-
-
-
-
-default_hostname = '127.0.0.1' # change it if needed
-default_port = 7497   # change it if needed
-default_client_id = 10645 # change it if needed
-
-
 reqId_serial = 1
-
 
 currencies = ['USD', 'EUR', 'GBP', 'JPY', 'AUD']
 
-
+image = 'asset/currency.jpeg'
+test_base64 = base64.b64encode(open(image, 'rb').read()).decode('ascii')
 
 app = dash.Dash(__name__)
+server = app.server
 
+app.layout = html.Div([
 
+    html.Div([html.H1('Acceptable Currency')], style={'color': 'blue', 'fontSize': 14, 'textAlign': 'center',
+                                                      'marginBottom': 50, 'marginTop': 25}),
 
+    html.Div([
+        html.Img(src='data:image/png;base64,{}'.format(test_base64))],
+        style={'textAlign': 'center', 'height': '10%', 'width': '10%'}),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Button('Arbitrage', id='check-button', n_clicks=0,
+                style={
+                    'marginRight': '50px',
+                    'textAlign': 'center',
+                    'position': 'absolute',
+                    'left': '50%',
+                    'transform': 'translateX(-50%)',
+                    'display': 'inline-block',
+                    'width': '50%',
 
-
-html.Div([
-
-    html.H4('Acceptable Currency'),
-    html.Img(src="asset/currency.jpeg"),
-    html.Button('Arbitrage', id='check-button', n_clicks=0),
+                }),
     html.Div(
         id='my_output1'
     ),
     html.Div(
-            id='my_output2'
-        )
+        id='my_output2')
 
-])
+   ])
 
 
 @app.callback(
@@ -81,7 +80,7 @@ html.Div([
 
 
     # Only run this callback function when the trade-button is pressed
-    Input('trade-button', 'n_clicks'),
+    Input('check-button', 'n_clicks'),
 
     prevent_initial_call=True
 )
@@ -94,83 +93,25 @@ def trade(n_clicks):
 
         exchange_dataframe = fill_in_nan(exchange_dataframe)
 
-        exchange_table = check_all_data(exchange_dataframe)
+        if not check_all_data(exchange_dataframe):
+            print('Exchange Data Unavailable')
+            exit(1)
 
-        data = exchange_table.to_dict('rows')
-        columns =  [{"name": i, "id": i,} for i in (exchange_table.columns)]
+        #exchange_table = check_all_data(exchange_dataframe)
+
+        data = exchange_dataframe.to_dict('rows')
+        columns =  [{"name": i, "id": i,} for i in (exchange_dataframe.columns)]
 
         results = dict()
-        results = check_all_arbitrage(results, exchange_table, currencies)
+        results = check_all_arbitrage(results, exchange_dataframe, currencies)
 
         optimal_route_str = max(results, key=results.get)
 
-
-
-
-
-        currency1 = optimal_route_str.split(',')[0]
-        currency2 = optimal_route_str.split(',')[1]
-        currency3 = optimal_route_str.split(',')[2]
-
-
-
-        contract1 = Contract()
-        contract1.symbol = currency1
-        contract1.secType = 'CASH'
-        contract1.currency = currency2
-        contract1.exchange = 'IDEALPRO'
-
-        order1 = Order()
-        order1.action = 'BUY'
-        order1.orderType = 'MKT'
-        order1.totalQuantity =  ibapi.wrapper.EWrapper.updateAccountValue('BuyingPower',currency1,default_client_id)
-
-        c1 = place_order(contract1, order1)
-
-
-        contract2 = Contract()
-        contract2.symbol = currency2
-        contract2.secType = 'CASH'
-        contract2.currency = currency3
-        contract2.exchange = 'IDEALPRO'
-
-        order2 = Order()
-        order2.action =  'BUY'
-        order2.orderType =  'MKT'
-        order2.totalQuantity =  ibapi.wrapper.EWrapper.updateAccountValue('BuyingPower',currency2,default_client_id)
-
-
-        c2 = place_order(contract2, order2)
-
-
-        contract3 = Contract()
-        contract3.symbol = currency3
-        contract3.secType = 'CASH'
-        contract3.currency = currency1
-        contract3.exchange = 'IDEALPRO'
-
-        order3 = Order()
-        order3.action =  'BUY'
-        order3.orderType =  'MKT'
-        order3.totalQuantity =  ibapi.wrapper.EWrapper.updateAccountValue('BuyingPower',currency3,default_client_id)
-
-
-
-        c3 = place_order(contract3, order3)
-
-
-        msg = html.Div(id='optimal route', children= optimal_route_str,
-                       style={'margin-bottom': '50px', 'text-align': 'center'}),
-
-
-        return dt.DataTable(data=data, columns=columns), msg
+        return dash_table.DataTable(data=data, columns=columns), optimal_route_str
 
     else:
         return "please click the button"
 
 
-
-
-    #msg = html.Div(id='optimal route', children= # return of optimal_route_function
-    #, style={'margin-bottom': '50px', 'text-align': 'center'}),
-
+if __name__ == '__main__':
+    app.run_server(debug=True)
